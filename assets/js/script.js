f4bde7c0b7d8fc9ee6fb146e47bc0547d8be86cf
Phuong Nguyen <tronganhnguyen222@gmail.com>
2024-03-19 11:52:20 +0700

    [UPDATE][FE] rollback

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 function getHotCoffee(){
   fetch("https://api.sampleapis.com/coffee/hot")
    .then(function(res){
@@ -8,8 +7,6 @@ function getHotCoffee(){
      getCoffeeItem(output)
    })
 }
-
-getHotCoffee()
 
 function getHotCoffeeById(id){
   fetch(`https://api.sampleapis.com/coffee/hot/${id}`)
@@ -50,10 +47,6 @@ function getCoffeeItem(output){
    document.querySelector("#hot-coffee").innerHTML = text
 }
 
-function goBack(){
-  window.location.href = "hotCoffee.html"
-}
-
 function getDetailHotCoffeeById(){
   let text = "<div class='row'>"
    text = `<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -62,9 +55,9 @@ function getDetailHotCoffeeById(){
        <h2 class="text-center text-primary responsive-text">${localStorage.getItem("title")}</h2>
        <span class="text-secondary text-center p-2">${localStorage.getItem("description")}</span>
        <div class="d-flex justify-content-center">
-         <button class="btn btn-info w-25 m-2 p-2" onclick="goBack()">
+         <a class="btn btn-info w-25 m-2 p-2" href="hotCoffee.html">
            <ion-icon name="arrow-back-outline"></ion-icon>
-         </button>
+         </a>
          <a class="btn btn-secondary text-white m-2 p-2" href="tel:0961847448">
            <ion-icon name="call-outline" role="img" class="md hydrated"></ion-icon> 0961847448
          </a>
@@ -74,8 +67,10 @@ function getDetailHotCoffeeById(){
   text += "</div>"
   document.querySelector("#coffee-detail").innerHTML = text
 }
-getDetailHotCoffeeById()
 
 function goToHomePage(){
- window.location.href = "index.html"
+  window.location.href = "index.html"
 }
+
+getHotCoffee()
+getDetailHotCoffeeById()
